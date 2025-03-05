17e920c58b264cf80e13cc78041ae797fce60db2
Geoo Chi <geoochi@geoochi.com>
2025-03-05 21:50:51 +0800

    update

---
+++ src/components/theme-blank.tsx
@@ -0,0 +1,8 @@
+import useTheme from '@/hooks/use-theme'
+
+const ThemeBlank: React.FC = () => {
+  useTheme()
+  return <></>
+}
+
+export default ThemeBlank
--- src/hooks/use-theme.ts
+++ src/hooks/use-theme.ts
@@ -6,29 +6,18 @@ function useTheme() {
   const [theme, setTheme] = useState<Theme>('light')
 
   useEffect(() => {
-    const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
-    setTheme(isDark ? 'dark' : 'light')
-
-    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
-
-    const handleChange = (e: MediaQueryListEvent) => {
-      setTheme(e.matches ? 'dark' : 'light')
+    if (localStorage.getItem('theme')) {
+      setTheme(localStorage.getItem('theme') as Theme)
+    } else {
+      const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+      setTheme(isDark ? 'dark' : 'light')
     }
-
-    mediaQuery.addEventListener('change', handleChange)
-
-    return () => mediaQuery.removeEventListener('change', handleChange)
   }, [])
 
   useEffect(() => {
-    // 保存主题到 localStorage
     localStorage.setItem('theme', theme)
-
-    if (theme === 'dark') {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
+    if (theme === 'dark') document.documentElement.classList.add('dark')
+    else document.documentElement.classList.remove('dark')
   }, [theme])
 
   return {
